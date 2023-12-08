1608bf8e0002ebeab46dd14445298a1be7412719
Shenya De Silva <shenya44@outlook.com>
2023-12-08 01:07:12 +0100

    Added function for naive approach, need to fix the intermediate function

--- launch_mlx.c
+++ launch_mlx.c
@@ -1,86 +1,84 @@
 #include "fdf.h"
 #include <stdio.h>
 
-/*
-void draw_pixel(uint8_t* pixel, uint32_t color)
-{
-	*(pixel++) = (uint8_t)(color >> 24);
-	*(pixel++) = (uint8_t)(color >> 16);
-	*(pixel++) = (uint8_t)(color >> 8);
-	*(pixel++) = (uint8_t)(color & 0xFF);
-}*/
-
 void make_pixel(uint8_t* pixel, uint32_t color)
 {
-	printf("\ncolor: %d\n", color);
-	printf("\npixel: %d\n", *(pixel));
 	*(pixel++) = (uint8_t)(color >> 24);
-	//printf("color: %d\n", color);
-	//printf("pixel: %d\n", *(pixel));
 	*(pixel++) = (uint8_t)(color >> 16);
 	*(pixel++) = (uint8_t)(color >> 8);
 	*(pixel++) = (uint8_t)(color & 0xFF);
 }
 
-/*
-mlx_image_t *draw_line(int X0, int Y0, int X1, int Y1, mlx_image_t *g_img) 
-{ 
-    // calculate dx & dy 
-    uint8_t* pixelstart;
-    int dx = X1 - X0; 
-    int dy = Y1 - Y0; 
-  
-    // calculate steps required for generating pixels 
-    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy); 
-  
-    // calculate increment in x & y for each steps 
-    float Xinc = dx / (float)steps; 
-    float Yinc = dy / (float)steps; 
-  
-    // Put pixel for each step 
-    float X = X0; 
-    float Y = Y0; 
-    for (int i = 0; i <= steps; i++) { 
-		pixelstart= &g_img->pixels[((int)round(Y) * g_img->width + (int)round(X)) * sizeof(int32_t)];
-		draw_pixel(pixelstart, 0xFFFFFFFF);
-    	//mlx_put_pixel(g_img, round(X), round(Y), 0xFFFFFFFF);
-        //putpixel(round(X), round(Y), RED); // put pixel at (X,Y) 
-        X += Xinc; // increment in x at each step 
-        Y += Yinc; // increment in y at each step 
-        //usleep(100); // for visualization of line- 
-                    // generation step by step 
-    } 
-    return (g_img);
-} */
-
-mlx_image_t *draw_line(int x0, int y0, int x1, int y1, mlx_image_t *g_img) 
+/* Bresenham's Line Algorithm */
+mlx_image_t *draw_line(t_line *line, mlx_image_t *g_img) 
 {
-	(void)x0;
-	(void)y0;
-	(void)x1;
-	(void)y1;
+	uint8_t*	pixelstart;
+	int x = 0;
+	int y = 0;
+	float m = 0;
+
+	m = (line->y1 - line->y0) / (line->x1 - line->x0);
+	for (x = line->x0; x <= line->x1; x++) {  
+        y = round(m*x);
+        if (x <= WIDTH && y <= HEIGHT && (x >= 0 && y >= 0))
+        {
+			pixelstart = &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
+			make_pixel (pixelstart, 0xFADD8E6);
+		}
+	}
+
 	return (g_img);
 }
+//uint8_t*	pixelstart;
+//pixelstart = &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
+//make_pixel (pixelstart, 0xFADD8E6);
+
+void make_line_zero(t_line *line)
+{
+	line->x0 = 0;
+	line->y0 = 0;
+	line->x1 = 0;
+	line->y1 = 0;
+	line->dx = 0;
+	line->dy = 0;
+	line->sx = 0;
+	line->sy = 0;
+	line->e = 0;
+	line->e2 = 0;
+}
+
 
 mlx_image_t *draw_image(t_screen *scrn, mlx_image_t *g_img)
 {
-	int i;
-	int j;
+	int 	i;
+	int 	j;
+	t_line	line;
 
 	i = 0;
 	j = 0;
+	make_line_zero(&line);
 	while (i < (scrn->x_max * scrn->y_max))
 	{
 		j = i + 1;
 		if (((i + 1) % scrn->x_max))
-			g_img=draw_line(scrn->iso[i].x, scrn->iso[i].y, scrn->iso[j].x, scrn->iso[j].y, g_img);
+		{
+			line.x0 = scrn->iso[i].x;
+			line.y0 = scrn->iso[i].y;
+			line.x1 = scrn->iso[j].x;
+			line.y1 = scrn->iso[j].y;
+			g_img=draw_line(&line, g_img);
+		}
 		i ++;
 	}
 	i = 0;
 	while (i < (scrn->x_max * scrn->y_max) - scrn->x_max)
 	{
 		j = i + scrn->x_max;
-		g_img=draw_line(scrn->iso[i].x, scrn->iso[i].y, scrn->iso[j].x, scrn->iso[j].y, g_img);
+		line.x0 = scrn->iso[i].x;
+		line.y0 = scrn->iso[i].y;
+		line.x1 = scrn->iso[j].x;
+		line.y1 = scrn->iso[j].y;
+		//g_img=draw_line(&line, g_img);
 		i ++;
 	}
 	return(g_img);
@@ -147,63 +145,49 @@ void print_image_details(mlx_image_t *g_img)
 	//printf("g_img->pixels[i]: %i\n", g_img->pixels[1]);
 }
 
-//print_image_details(g_img);
-
 mlx_image_t *draw_dots_to_image(mlx_image_t *g_img, t_screen *scrn)
 {
-	int i = 0;
-	int x = 0;
-	int y = 0;
-	uint8_t* pixelstart;
+	int			i;
+	int			x;
+	int			y;
+	uint8_t*	pixelstart;
+
+	i = 0;
+	x = 0;
+	y = 0;
 	while (i < (scrn->x_max * scrn->y_max))
 	{
 		x = (scrn->iso[i].x);
 		y = (scrn->iso[i].y);
 		if (x >= scrn->z_min && y >= scrn->z_max && y <= HEIGHT - HEIGHT/3 && x <= WIDTH - WIDTH/5)
 		{
-			//pixelstart= &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
-			pixelstart= &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
-			printf("(%d,%d) ,",x, y);
-			printf("(%f,%f) ,",scrn->iso[i].x, scrn->iso[i].y);
-			//mlx_put_pixel(g_img, x, y, 0xFFFFFFFF);
-			//mlx_put_pixel(g_img, (scrn->iso[i].x), scrn->iso[i].y, 0xFFFFFFFF);
-			//draw_pixel(pixelstart, 0xFFFFFFFF);
-			make_pixel(pixelstart, 0xFADD8E6);
+			pixelstart = &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
+			make_pixel (pixelstart, 0xFADD8E6);
+			printf("%d: pixp: %p\n",i, pixelstart);
+			printf("%d: pixd: %p\n",i, &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)]);
 		}
 		i ++;
 	}
-	/**
- * Sets / puts a pixel onto an image.
- * 
- * NOTE: It is considered undefined behaviour when putting a pixel 
- * beyond the bounds of an image.
- * 
- * @param[in] image The MLX instance handle.
- * @param[in] x The X coordinate position.
- * @param[in] y The Y coordinate position.
- * @param[in] color The color value to put.
- */
-//void mlx_put_pixel(mlx_image_t* image, uint32_t x, uint32_t y, uint32_t color);
 	return (g_img);
 }
 
 int launch_mlx_window(t_screen	*map)
 {
-	mlx_t *mlx; // mlx.display = display
-	mlx_image_t *g_img;
+	mlx_t		*mlx;
+	mlx_image_t	*g_img;
 	
 	mlx = mlx_init(WIDTH, HEIGHT, "Wireframe", true);
 	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);   // Creates a new image.
 
-	g_img = draw_dots_to_image(g_img, map);
-	printf("\ng_img->width: %d\n",g_img->width);
-	printf("\ng_img->height: %d\n",g_img->height);
-    //g_img = draw_image(map, g_img);
+	//g_img = draw_dots_to_image(g_img, map);
+	//printf("\ng_img->width: %d\n",g_img->width);
+	//printf("\ng_img->height: %d\n",g_img->height);
+    g_img = draw_image(map, g_img);
     mlx_image_to_window(mlx, g_img, WIDTH/5, HEIGHT/3);  // Adds an image to the render queue.
     //draw_verts(map, g_img);
     //draw_image(map, g_img);
     //mlx_image_to_window(mlx, g_img, 100, HEIGHT/3);  // Adds an image to the render queue.
-	ft_printf("cols :%d\nrows :%d\n", (*map).x_max, (*map).y_max);
+	//ft_printf("cols :%d\nrows :%d\n", (*map).x_max, (*map).y_max);
 	// Connect the dots here
 	mlx_loop(mlx);
 	//free(g_img->pixels);
