0d00a6d42af81c5ed3233a9d80fa6ead60f3cd90
Shenya De Silva <shenya44@outlook.com>
2023-12-08 23:33:58 +0100

    My own working breseneham function YAY

--- launch_mlx.c
+++ launch_mlx.c
@@ -9,24 +9,61 @@ void make_pixel(uint8_t* pixel, uint32_t color)
 	*(pixel++) = (uint8_t)(color & 0xFF);
 }
 
-/* Bresenham's Line Algorithm */
-mlx_image_t *draw_line(t_line *line, mlx_image_t *g_img) 
+mlx_image_t *line_helper(int x, int y, t_line *line, mlx_image_t *g_img, t_screen *scrn)
 {
 	uint8_t*	pixelstart;
-	int x = 0;
-	int y = 0;
-	float m = 0;
 
-	m = (line->y1 - line->y0) / (line->x1 - line->x0);
-	for (x = line->x0; x <= line->x1; x++) {  
-        y = round(m*x);
-        if (x <= WIDTH && y <= HEIGHT && (x >= 0 && y >= 0))
+	while (1)
+	{
+		x = line->x0; y = line->y0;
+		//if (x <= WIDTH && y <= HEIGHT && (x >= 0 && y >= 0))
+		if (x >= scrn->z_min && y >= scrn->z_max && y <= HEIGHT - HEIGHT/3 && x <= WIDTH - WIDTH/5)
         {
 			pixelstart = &g_img->pixels[(y * g_img->width + x) * sizeof(int32_t)];
 			make_pixel (pixelstart, 0xFADD8E6);
 		}
+		if (line->x0 == line->x1 && line->y0 == line->y1)
+			break;
+		line->e2 = line->e * 2;
+		if (line->e2 >= line->dy)
+		{
+			if (line->x0 == line->x1)
+				break;
+			line->e = line->e + line->dy;
+			line->x0 = line->x0 + line->sx;
+		}
+		if (line->e2 <= line->dx)
+		{
+			if (line->y0 == line->y1)
+				break;
+			line->e = line->e + line->dx;
+			line->y0 = line->y0 + line->sy;
+		}
 	}
+	return(g_img);
+}
 
+/* Bresenham's Line Algorithm */
+mlx_image_t *draw_line(t_line *line, mlx_image_t *g_img, t_screen *scrn) 
+{
+	
+	int 		x;
+	int 		y;
+
+	x = 0;
+	y = 0;
+	line->dx = abs(line->x1 - line->x0);
+	if (line->x0 < line->x1)
+		line->sx = 1;
+	else
+		line->sx = -1;
+	line->dy = -abs(line->y1 - line->y0);
+	if (line->y0 < line->y1)
+		line->sy = 1;
+	else
+		line->sy = -1;
+	line->e = line->dx + line->dy;
+	g_img = line_helper(x, y, line, g_img, scrn);
 	return (g_img);
 }
 //uint8_t*	pixelstart;
@@ -60,13 +97,13 @@ mlx_image_t *draw_image(t_screen *scrn, mlx_image_t *g_img)
 	while (i < (scrn->x_max * scrn->y_max))
 	{
 		j = i + 1;
-		if (((i + 1) % scrn->x_max))
+		if (j % scrn->x_max)
 		{
 			line.x0 = scrn->iso[i].x;
 			line.y0 = scrn->iso[i].y;
 			line.x1 = scrn->iso[j].x;
 			line.y1 = scrn->iso[j].y;
-			g_img=draw_line(&line, g_img);
+			g_img=draw_line(&line, g_img, scrn);
 		}
 		i ++;
 	}
@@ -78,7 +115,7 @@ mlx_image_t *draw_image(t_screen *scrn, mlx_image_t *g_img)
 		line.y0 = scrn->iso[i].y;
 		line.x1 = scrn->iso[j].x;
 		line.y1 = scrn->iso[j].y;
-		//g_img=draw_line(&line, g_img);
+		g_img=draw_line(&line, g_img, scrn);
 		i ++;
 	}
 	return(g_img);
