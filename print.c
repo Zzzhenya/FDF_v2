0cdf4c6a2feade7e3999ebd3c0aae702a0c329c7
Shenya De Silva <shenya44@outlook.com>
2023-12-14 17:39:54 +0100

    Initial commit print.c

---
+++ print.c
@@ -0,0 +1,45 @@
+#include "fdf.h"
+
+void	print_t_cord(t_screen *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->x_max * map->y_max)
+	{
+		ft_printf("cord: %d,(%d,%d,%d)\n", i,
+			map->cord[i].x,
+			map->cord[i].y,
+			map->cord[i].z);
+		i ++;
+	}
+}
+
+void	print_screen_details(t_screen *scrn)
+{
+	printf("alpha: %f\n", scrn->a);
+	printf("beta: %f\n", scrn->b);
+	ft_printf("x_max :%d\n", scrn->x_max);
+	ft_printf("y_max :%d\n", scrn->y_max);
+	ft_printf("ymin :%d\n", scrn->y_min);
+	ft_printf("xmin :%d\n", scrn->x_min);
+	if (scrn->cord)
+		print_t_cord(scrn);
+	if (scrn->iso)
+		print_t_vert(scrn);
+}
+
+void	print_t_vert(t_screen *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->x_max * map->y_max)
+	{
+		printf ("vert: %d,(%f,%f,%f)\n", i,
+			map->iso[i].x,
+			map->iso[i].y,
+			map->iso[i].z);
+		i ++;
+	}
+}
