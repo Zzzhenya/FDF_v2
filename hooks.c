2b502312b9117561e5678131eaf2e7e425c916b1
Shenya De Silva <sde-silv@c4b11c5.42berlin.de>
2023-12-16 16:37:41 +0100

    Added header

--- hooks.c
+++ hooks.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hooks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 16:34:09 by sde-silv          #+#    #+#             */
+/*   Updated: 2023/12/16 16:37:10 by sde-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
 void	my_keyhook(mlx_key_data_t keydata, void *map)
 {
-	t_screen *scrn;
+	t_screen	*scrn;
 
 	scrn = (t_screen *)map;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
