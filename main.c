75c71272237e6ba098c3f431311f2e4e245a0e5e
Shenya De Silva <shenya44@outlook.com>
2023-12-20 21:59:00 +0100

    Moved make_line_zero to main and set exit code to 1

--- main.c
+++ main.c
@@ -12,6 +12,20 @@
 
 #include "fdf.h"
 
+static void	make_line_zero(t_line *line)
+{
+	line->x0 = 0;
+	line->y0 = 0;
+	line->x1 = 0;
+	line->y1 = 0;
+	line->dx = 0;
+	line->dy = 0;
+	line->sx = 0;
+	line->sy = 0;
+	line->e = 0;
+	line->e2 = 0;
+}
+
 t_screen	*init_screen(t_screen	*scrn)
 {
 	scrn = malloc (sizeof (t_screen));
@@ -30,27 +44,32 @@ t_screen	*init_screen(t_screen	*scrn)
 	scrn->cord = NULL;
 	scrn->mlx = NULL;
 	scrn->g_img = NULL;
+	make_line_zero (&scrn->line);
 	return (scrn);
 }
 
 int	main(int argc, char **argv)
 {
 	t_screen	*scrn;
+	int			code;
 
+	code = 0;
 	scrn = NULL;
 	if (argc != 2)
 	{
-		ft_printf ("	Usage: ./fdf *.fdf\n");
+		ft_printf ("Usage: ./fdf map.fdf\n");
 		return (1);
 	}
 	scrn = init_screen (scrn);
 	if (!scrn)
 		ft_errexit("t_screen init error");
 	parse_and_store (scrn, argv[1]);
-	fdf_init (scrn, 1);
-	launch_mlx_window(scrn, argv[1]);
+	fdf_init (scrn);
+	code = launch_mlx_window(scrn, argv[1]);
 	free_coord(scrn->cord);
 	free_vert(scrn->iso);
 	free_screen (scrn);
+	if (code != 0)
+		ft_errexit ("MLX failed.");
 	return (0);
 }
